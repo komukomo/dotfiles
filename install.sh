22846923c78cb9289c94c3f7af64b77f201470b3
komukomo <komukomo@users.noreply.github.com>
2016-03-26 18:55:48 +0900

    Enable insall by curl

--- install.sh
+++ install.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+#
+# @(#) install.sh  - install dotfiles
+#
 
-DIRPATH=$(cd `dirname $0`; pwd)
-cd ${DIRPATH}
+DOT_DIR="$HOME/.kmkm-dotfiles"
 
-TARGETS=$(git ls-files ".*")
+main() {
+	echo "run install.sh ..."
+	get_repo
+	do_install
+}
 
-for file in ${TARGETS[@]}
-do
-    ln -s ${DIRPATH}/${file} ${HOME}/${file}
-    if [ $? -eq 0 ]; then
-        echo "success to create sym link: ${file}"
-    fi
-done
+get_repo() {
+	if [ ! -e ${DOT_DIR} ]; then
+		git clone https://github.com/komukomo/dotfiles.git ${DOT_DIR}
+	else
+		echo "${DOT_DIR} already exists."
+	fi
+}
+
+do_install() {
+	DIRPATH=$(cd `dirname $0`; pwd)
+	cd ${DIRPATH}
+
+	TARGETS=$(git ls-files ".*")
+
+	for file in ${TARGETS[@]}
+	do
+		ln -s ${DIRPATH}/${file} ${HOME}/${file}
+		if [ $? -eq 0 ]; then
+			echo "success to create sym link: ${file}"
+		fi
+	done
+}
+
+main
