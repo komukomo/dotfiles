2dac18fe351499075738f7c3cbc4afea1704bf31
komukomo <komukomo@users.noreply.github.com>
2017-08-06 17:35:18 +0900

    [fzf] Add fzf settings

---
+++ .fzf.zsh
@@ -0,0 +1,34 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+
+# Customize
+# ------------
+bindkey -r '\ec'
+bindkey '^G' fzf-cd-widget
+
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+
+# To apply the command to CTRL-T as well
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+# https://github.com/junegunn/fzf/wiki/examples
+# fbr - checkout git branch (including remote branches)
+fbr() {
+  local branches branch
+  branches=$(git branch --all | grep -v HEAD) &&
+  branch=$(echo "$branches" |
+           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
